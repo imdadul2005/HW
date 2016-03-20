7db5456b27f437ebef37f59464a1447a588b226b
imdadulhoq <shamol2@gmail.com>
2016-03-20 16:17:41 -0400

    Adding random number generator with array

---
+++ arrayWithRand.java
@@ -0,0 +1,30 @@
+package variable;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class arrayWithRand {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+     	System.out.println("Type a max random number ");
+		int maxLimit = input.nextInt();
+		Random rand = new Random();
+	
+		
+		int [] array = new int[10];
+		for (int i=0; i<array.length;i++)
+			array[i]=rand.nextInt(maxLimit);
+		
+		int counter = 0;
+		while(counter<array.length){
+			System.out.println("array["+  counter +"] = "+ array[counter] );
+			counter++;
+		}
+		input.close();
+	}
+	
+
+}
